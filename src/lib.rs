785636638a13dd7b0b061c79178a951344ecb8f5
Autumn <autumn@puffin.place>
2024-08-23 03:04:24 +1000

    Initial crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-pub use error::{Error, Result};
-
+//! TIDAL-rs is a wrapper for the Tidal API written in Rust, aiming to provide a simple interface
+//! for interacting with the Tidal API.
+//! 
+//! The library is designed to be as flexible as possible, allowing the user to choose how they 
+//! want to handle authentication, and how they want to store credentials.
+//! 
+//! There are a few clients implimented in the TIDAL-rs, Including the `Client` struct, which is a
+//! simple blocking client that impliments all of the traits available, aswell as standalone
+//! clients for individual parts of the API, such as the `AuthClient` which impl impliments the
+//! `Auth` trait and authentication flows, and the `CatalogueClient` which impliments the
+//! `Catalogue` trait and provides access to the Tidal catalogue, such as searching for tracks,
+//! albums, and artists.
 
 /// Module for the Tidal API client
 pub mod client;
@@ -16,6 +26,7 @@ pub(crate) mod utils;
 
 
 pub mod error;
+pub use error::{Error, Result};
 
 #[cfg(test)]
 mod tests;
