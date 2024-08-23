cdf49aa64c73d9177d2cbfd6f46715c362cd3187
Autumn <autumn@puffin.place>
2024-08-24 00:27:25 +1000

    Add endpoints for Users

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -17,17 +17,23 @@ pub mod base_urls {
 }
 
 #[derive(Debug, Clone, Copy)]
-pub enum Endpoint {  
+pub enum Endpoint<'a> {  
   OAuth2Token,
   OAuth2DeviceAuth,
   LoginAuthorize,
+  Users(&'a u64),
+  UsersSubscription(&'a u64),
+  UsersClients(&'a u64),
 }
-impl Endpoint {
+impl Endpoint<'_> {
   pub fn to_string(&self) -> String {
     let (base, path) = match self {
-      Self::OAuth2Token => (base_urls::AUTH_URL, "oauth2/token"),
-      Self::OAuth2DeviceAuth => (base_urls::AUTH_URL, "oauth2/device_authorization"),
-      Self::LoginAuthorize => (base_urls::LOGIN_URL, "authorize"),
+      Self::OAuth2Token => (base_urls::AUTH_URL, "oauth2/token".to_owned()),
+      Self::OAuth2DeviceAuth => (base_urls::AUTH_URL, "oauth2/device_authorization".to_owned()),
+      Self::LoginAuthorize => (base_urls::LOGIN_URL, "authorize".to_owned()),
+      Self::Users(id) => (base_urls::API_URL_V1, format!("users/{id}")),
+      Self::UsersSubscription(id) => (base_urls::API_URL_V1, format!("users/{id}/subscription")),
+      Self::UsersClients(id) => (base_urls::API_URL_V1, format!("users/{id}/clients")),
     };
     format!("{}{}", base, path)
   }
