764c1d2273990fdb00a475db4247994ed4d9b8e0
Autumn <autumn@puffin.place>
2024-08-26 03:12:14 +1000

    break authorization into its own fn in examples

--- examples/get_current_user_info.rs
+++ examples/get_current_user_info.rs
@@ -9,14 +9,18 @@ fn get_client() -> Client {
   Client::new(ClientCreds::new(dotenv!("ClientID").to_owned(), dotenv!("ClientSecret").to_owned()))
 }
 
-// Note: This example requires client credentials capable of device flow (i.e. an android tv client)
-fn main() {
-  let mut client = get_client();
+fn auth(client: &mut Client) {
   client.set_country(isocountry::CountryCode::AUS);
   let dev_res = client.device_login_init().unwrap();
   println!("Please Go To https://{}\n", dev_res.verification_uri_complete);
   client.device_login_finalize(&dev_res).unwrap();
   println!("Logged in as: {:?}\n", client.get_credentials().unwrap().user_id());
+}
+
+// Note: This example requires client credentials capable of device flow (i.e. an android tv client)
+fn main() {
+  let mut client = get_client();
+  auth(&mut client);
 
   let user = client.get_current_user().unwrap();
   println!("User Info: {:#?}\n", user);
