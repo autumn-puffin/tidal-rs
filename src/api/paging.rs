19728a831003812f3470cff69de9c19d5d21ebf9
Autumn <autumn@puffin.place>
2024-09-11 04:11:13 +1000

    flatten Paging into PagingList

--- src/api/paging.rs
+++ src/api/paging.rs
@@ -13,10 +13,7 @@ pub struct Paging<T> {
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PagingList<T> {
-  pub limit: u64,
-  pub offset: u64,
-  #[serde(rename = "totalNumberOfItems")]
-  pub total_items: u64,
-  pub items: Vec<T>,
+  #[serde(flatten)]
+  pub paging: Paging<T>,
   pub data_api_path: Option<String>,
 }
