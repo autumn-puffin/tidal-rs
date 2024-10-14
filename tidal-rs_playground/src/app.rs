d985bb3db5076d27b0198d35109dd27c400194a9
Autumn <autumn@puffin.place>
2024-10-14 17:20:43 +1100

    extract out panel components and swap out work on Catalogue interactions panel

--- tidal-rs_playground/src/app.rs
+++ tidal-rs_playground/src/app.rs
@@ -1,7 +1,7 @@
 use crate::{AppEvent, BackgroundEvent};
-use egui::{RichText, TextEdit};
+use egui::{Label, RichText, TextEdit};
 use std::sync::{mpsc, Arc, Mutex};
-use tidal_rs::{api::Page, client::Client};
+use tidal_rs::client::Client;
 
 mod custom_ui;
 use custom_ui::*;
@@ -27,7 +27,7 @@ impl App {
   fn handle_events(&mut self) {
     while let Ok(event) = self.event_receiver.try_recv() {
       match event {
-        AppEvent::SetCataloguePage(page) => self.app_state.interface_state.page = Some(page),
+        AppEvent::SetCataloguePage(page) => self.app_state.interface_state.page = Some(format!("{page:#?}")),
       }
     }
   }
@@ -40,35 +40,37 @@ impl App {
       .on_hover_text("Authenticates with the device flow")
       .clicked()
       .then(|| self.event_sender.send(BackgroundEvent::AuthWithDeviceFlow).unwrap());
-    egui::ScrollArea::horizontal().max_width(250.).show(ui, |ui| {
-      egui::Grid::new("auth_grid").num_columns(2).striped(true).show(ui, |ui| {
-        let client = self.client.lock().unwrap();
-        kv_row(ui, ("Client ID", client.get_client_credentials().id()));
-        kv_row(ui, ("Client Secret", client.get_client_credentials().secret()));
+    egui::ScrollArea::horizontal().max_width(250.).show(ui, |ui| self.auth_grid(ui));
+  }
+  fn auth_grid(&self, ui: &mut egui::Ui) {
+    egui::Grid::new("auth_grid").num_columns(2).striped(true).show(ui, |ui| {
+      let client = self.client.lock().unwrap();
+      kv_row(ui, ("Client ID", client.get_client_credentials().id()));
+      kv_row(ui, ("Client Secret", client.get_client_credentials().secret()));
 
-        if let Some(auth_creds) = client.get_auth_credentials() {
-          kv_row(ui, ("Access Token", auth_creds.access_token()));
-          kv_row(ui, ("Refresh Token", auth_creds.refresh_token().unwrap_or("None")));
-          let expires_at = chrono::DateTime::from_timestamp(auth_creds.expires_at(), 0)
-            .map(|dt| dt.to_string())
-            .unwrap_or("None".to_string());
-          kv_row(ui, ("Expires At", expires_at));
-          if let Some(user) = auth_creds.auth_user() {
-            kv_row(ui, ("Username", &user.username));
-            kv_row(ui, ("User ID", user.user_id.to_string()));
-            kv_row(ui, ("User Email", &user.email));
-            kv_row(ui, ("User Country", user.country_code.name()));
-          } else {
-            ui.label("No User");
-            ui.end_row();
-          }
+      if let Some(auth_creds) = client.get_auth_credentials() {
+        kv_row(ui, ("Access Token", auth_creds.access_token()));
+        kv_row(ui, ("Refresh Token", auth_creds.refresh_token().unwrap_or("None")));
+        let expires_at = chrono::DateTime::from_timestamp(auth_creds.expires_at(), 0)
+          .map(|dt| dt.to_string())
+          .unwrap_or("None".to_string());
+        kv_row(ui, ("Expires At", expires_at));
+        if let Some(user) = auth_creds.auth_user() {
+          kv_row(ui, ("Username", &user.username));
+          kv_row(ui, ("User ID", user.user_id.to_string()));
+          kv_row(ui, ("User Email", &user.email));
+          kv_row(ui, ("User Country", user.country_code.name()));
         } else {
-          ui.label("No Auth Creds");
+          ui.label("No User");
           ui.end_row();
         }
-      });
+      } else {
+        ui.label("No Auth Creds");
+        ui.end_row();
+      }
     });
   }
+
   fn draw_selection_panel(&mut self, ui: &mut egui::Ui) {
     let interface_state = &mut self.app_state.interface_state;
     ui.vertical(|ui| {
@@ -77,6 +79,7 @@ impl App {
       ui.selectable_value(&mut interface_state.selection, Some(InterfaceSelection::Track), "Track");
     });
   }
+
   fn draw_interactions_panel(&mut self, ui: &mut egui::Ui) {
     let interface_state = &mut self.app_state.interface_state;
     egui::ScrollArea::vertical().show(ui, |ui| match interface_state.selection {
@@ -88,15 +91,19 @@ impl App {
         ui.label(RichText::new("Catalogue").heading());
         ui.separator();
         ui.label("Get Page");
-        ui.text_edit_singleline(&mut interface_state.page_path);
-        ui.button("Get").on_hover_text("Get the specified page").clicked().then(|| {
-          println!("Getting page: {}", interface_state.page_path);
-          self
-            .event_sender
-            .send(BackgroundEvent::CatalogueGetPage(interface_state.page_path.clone()))
-            .unwrap();
+        ui.horizontal(|ui| {
+          ui.add(TextEdit::singleline(&mut interface_state.page_path).desired_width(100.));
+          ui.button("Get").on_hover_text("Get the specified page").clicked().then(|| {
+            println!("Getting page: {}", interface_state.page_path);
+            self
+              .event_sender
+              .send(BackgroundEvent::CatalogueGetPage(interface_state.page_path.clone()))
+              .unwrap();
+          })
         });
-        ui.add(TextEdit::multiline(&mut format!("{:#?}", interface_state.page)).code_editor());
+        egui::ScrollArea::vertical().max_height(250.).min_scrolled_height(250.).show(ui, |ui| {
+          egui::Frame::dark_canvas(ui.style()).show(ui, |ui| ui.add(Label::new(interface_state.page.clone().unwrap_or("No Page".to_owned()))));
+        });
       }
       Some(InterfaceSelection::Track) => {
         ui.label(RichText::new("Track").heading());
@@ -141,5 +148,5 @@ struct InterfaceState {
   selection: Option<InterfaceSelection>,
   // Catalogue
   page_path: String,
-  page: Option<Page>,
+  page: Option<String>,
 }
