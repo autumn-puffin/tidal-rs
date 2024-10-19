f3f03c955697c626842e3b133844c79dd1252eb7
Autumn <autumn@puffin.place>
2024-10-19 19:54:20 +1100

    Extract event handler to it's own function

--- tidal-rs_playground/src/main.rs
+++ tidal-rs_playground/src/main.rs
@@ -1,7 +1,10 @@
 use std::{
   fs::{self, File},
   io::Write as _,
-  sync::{mpsc, Arc, Mutex},
+  sync::{
+    mpsc::{self, Receiver, Sender},
+    Arc, Mutex,
+  },
 };
 
 use tidal_rs::client::{auth::AuthCreds, Auth, Catalogue, Client, ClientCreds, DeviceFlow};
@@ -12,39 +15,9 @@ fn main() -> eframe::Result {
   let (app_event_sender, app_event_receiver) = mpsc::channel::<AppEvent>();
 
   let client = Arc::new(Mutex::new(init_client()));
+  let event_handler_client = client.clone();
 
-  let client_mutex = client.clone();
-  std::thread::spawn(move || {
-    let receiver = background_event_receiver;
-    let sender = app_event_sender;
-    while let Ok(event) = receiver.recv() {
-      use BackgroundEvent::*;
-      match event {
-        AuthWithDeviceFlow => {
-          let mut client = client_mutex.lock().unwrap();
-          let dev_res = client.device_login_init().unwrap();
-          open::that_in_background(&dev_res.verification_uri_complete);
-          client.device_login_finalize(&dev_res).unwrap();
-          println!("Logged in as: {:?}\n", client.get_credentials().unwrap().user_id());
-          let auth = client.get_auth_credentials().unwrap();
-          let creds_json = serde_json::to_string_pretty(&auth).unwrap();
-          let mut file = File::create("auth.json").unwrap();
-          file.write_all(creds_json.as_bytes()).unwrap();
-        }
-        CatalogueGetPage(page) => {
-          let client = client_mutex.lock().unwrap();
-          let res = client.get_page(&page);
-          drop(client);
-          match res {
-            Ok(response) => {
-              sender.send(AppEvent::SetCataloguePage(response)).unwrap();
-            }
-            Err(e) => println!("Error getting page: {e:?}"),
-          }
-        }
-      }
-    }
-  });
+  std::thread::spawn(move || event_handler(event_handler_client, background_event_receiver, app_event_sender));
 
   let options = eframe::NativeOptions::default();
   eframe::run_native(
@@ -74,4 +47,34 @@ fn init_client() -> Client {
   }
 
   client
+}
+
+fn event_handler(client: Arc<Mutex<Client>>, receiver: Receiver<BackgroundEvent>, sender: Sender<AppEvent>) {
+  while let Ok(event) = receiver.recv() {
+    use BackgroundEvent::*;
+    match event {
+      AuthWithDeviceFlow => {
+        let mut client_locked = client.lock().unwrap();
+        let dev_res = client_locked.device_login_init().unwrap();
+        open::that_in_background(&dev_res.verification_uri_complete);
+        client_locked.device_login_finalize(&dev_res).unwrap();
+        println!("Logged in as: {:?}\n", client_locked.get_credentials().unwrap().user_id());
+        let auth = client_locked.get_auth_credentials().unwrap();
+        let creds_json = serde_json::to_string_pretty(&auth).unwrap();
+        let mut file = File::create("auth.json").unwrap();
+        file.write_all(creds_json.as_bytes()).unwrap();
+      }
+      CatalogueGetPage(page) => {
+        let client_locked = client.lock().unwrap();
+        let res = client_locked.get_page(&page);
+        drop(client_locked);
+        match res {
+          Ok(response) => {
+            sender.send(AppEvent::SetCataloguePage(response)).unwrap();
+          }
+          Err(e) => println!("Error getting page: {e:?}"),
+        }
+      }
+    }
+  }
 }
