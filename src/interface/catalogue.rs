c18c3dd5091d61b80b4b55bb9101563945654dc6
Autumn Aves <autumn@puffin.place>
2024-11-25 12:42:01 +1100

    Should have used format instead of url parsing

--- src/interface/catalogue.rs
+++ src/interface/catalogue.rs
@@ -20,15 +20,15 @@ pub trait Catalogue: Auth {
     self.get_page("explore")
   }
   fn get_mix_page(&self, mix_id: &str) -> Result<Page> {
-    let path = Url::parse_with_params("album", [("mixId", mix_id)])?;
+    let path = format!("mix?mixId={}", mix_id);
     self.get_page(path.as_str())
   }
   fn get_artist_page(&self, artist_id: &u64) -> Result<Page> {
-    let path = Url::parse_with_params("artist", [("artistId", artist_id.to_string())])?;
+    let path = format!("artist?artistId={}", artist_id.to_string());
     self.get_page(path.as_str())
   }
   fn get_album_page(&self, album_id: &u64) -> Result<Page> {
-    let path = Url::parse_with_params("album", [("albumId", album_id.to_string())])?;
+    let path = format!("album?albumId={}", album_id.to_string());
     self.get_page(path.as_str())
   }
 }
