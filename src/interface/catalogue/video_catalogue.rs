86d78c7564cf5b90826b74515fadd81a6b9762d0
Autumn <autumn@puffin.place>
2024-11-28 13:48:41 +1100

    get_video_reccommendations() should not have and offset

--- src/interface/catalogue/video_catalogue.rs
+++ src/interface/catalogue/video_catalogue.rs
@@ -4,6 +4,6 @@ use super::Catalogue;
 
 pub trait VideoCatalogue: Catalogue {
   fn get_video(&self, video_id: &u64) -> Result<api::Video>;
-  fn get_video_recommendations(&self, video_id: &u64, offset: &u64, limit: &u64) -> Result<api::Paging<api::MediaRecommendation>>;
+  fn get_video_recommendations(&self, video_id: &u64, limit: &u64) -> Result<api::Paging<api::MediaRecommendation>>;
   fn playback_info_for_video(&self, video_id: &u64, options: &api::PlaybackInfoOptions) -> Result<api::PlaybackInfo>;
 }
