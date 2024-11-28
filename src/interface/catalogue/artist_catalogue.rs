81c3245b060638196508f0e48c765ac4c36807a5
Autumn <autumn@puffin.place>
2024-11-28 14:32:41 +1100

    Add missing get_artist_albums() method

--- src/interface/catalogue/artist_catalogue.rs
+++ src/interface/catalogue/artist_catalogue.rs
@@ -8,4 +8,5 @@ pub trait ArtistCatalogue: Catalogue {
   fn get_artist_mix_id(&self, artist_id: &u64) -> Result<api::MixId>;
   fn get_artist_top_tracks(&self, artist_id: &u64, offset: &u64, limit: &u64) -> Result<api::Paging<api::Track>>;
   fn get_artist_videos(&self, artist_id: &u64, offset: &u64, limit: &u64) -> Result<api::Paging<api::Video>>;
+  fn get_artist_albums(&self, artist_id: &u64, offset: &u64, limit: &u64) -> Result<api::Paging<api::Album>>;
 }
