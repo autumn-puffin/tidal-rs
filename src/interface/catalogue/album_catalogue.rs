6ae05c5d0e0307a03930843c1bbff740baa14053
Autumn <autumn@puffin.place>
2024-11-28 15:11:30 +1100

    get_album_credits should return a vec

--- src/interface/catalogue/album_catalogue.rs
+++ src/interface/catalogue/album_catalogue.rs
@@ -4,7 +4,7 @@ use super::Catalogue;
 
 pub trait AlbumCatalogue: Catalogue {
   fn get_album(&self, album_id: &u64) -> Result<api::Album>;
-  fn get_album_credits(&self, album_id: &u64, include_contributors: bool) -> Result<api::MediaCredit>;
+  fn get_album_credits(&self, album_id: &u64, include_contributors: bool) -> Result<Vec<api::MediaCredit>>;
   fn get_album_items(&self, album_id: &u64, offset: &u64, limit: &u64) -> Result<api::Paging<api::MediaItem>>;
   fn get_album_items_with_credits(
     &self,
