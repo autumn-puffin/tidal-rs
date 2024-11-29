a08e248cf70a86dc0f8ae24d27e67df93a813e09
Autumn <autumn@puffin.place>
2024-11-30 10:53:49 +1100

    Fill fields for Video

--- src/api/media/video.rs
+++ src/api/media/video.rs
@@ -1,8 +1,29 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+
+use super::{Album, Artist};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Video {
+  pub id: u64,
+  pub title: String,
+  pub url: Option<String>,
+  pub track_number: u64,
+  pub volume_number: u64,
+  pub duration: u64,
+  pub popularity: u64,
+  pub double_popularity: Option<f64>,
+  pub stream_ready: bool,
+  pub ad_supported_stream_ready: bool,
+  pub allow_streaming: bool,
+  pub explicit: bool,
+  pub dj_ready: bool,
+  pub stem_ready: bool,
+  pub album: Option<Album>,
+  pub artists: Vec<Artist>,
+  pub stream_start_date: DateTime<Utc>,
+
   #[cfg(feature = "show_unmodeled")]
   #[serde(flatten)]
   pub unserialized: Box<std::collections::HashMap<String, serde_json::Value>>,
