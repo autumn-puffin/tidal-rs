aab5ea95cb101bcf0cc7e43720a1986a4d22dd90
Autumn <autumn@puffin.place>
2025-01-24 12:56:58 +1100

    Add PlaylistCollection for my-collection/playlists endpoints

--- src/interface/catalogue/playlist.rs
+++ src/interface/catalogue/playlist.rs
@@ -1,3 +1,4 @@
+use reqwest::Response;
 use uuid::Uuid;
 
 use crate::{api, Result};
@@ -8,4 +9,21 @@ pub trait PlaylistCatalogue: Catalogue {
   fn get_playlist(&self, playlist_id: &Uuid) -> Result<api::Playlist>;
   fn get_playlist_items(&self, playlist_id: &Uuid, offset: &u64, limit: &u64) -> Result<api::Paging<api::MediaItem>>;
   fn get_playlist_recommendations(&self, playlist_id: &Uuid, offset: &u64, limit: &u64) -> Result<api::Paging<api::MediaItem>>;
+}
+
+pub trait PlaylistCollection {
+  fn add_favorite_playlist(&self, playlist_id: &Uuid) -> Result<Response>;
+  fn create_playlist(&self, name: &str, description: &str, is_public: bool) -> Result<Response>;
+  fn remove_playlist(&self, playlist_id: &Uuid) -> Result<Response>;
+  fn move_playlist(&self, folder_id: &Uuid, playlist_id: &Uuid) -> Result<Response>;
+  fn edit_playlist(&self, playlist_id: &Uuid, name: &str, description: &str) -> Result<Response>;
+  fn publish_playlist(&self, playlist_id: &Uuid) -> Result<Response>;
+  fn unpublish_playlist(&self, playlist_id: &Uuid) -> Result<Response>;
+  fn get_collection_playlist(&self, playlist_id: &Uuid) -> Result<Response>;
+
+  fn create_folder(&self, name: &str) -> Result<Response>;
+  fn remove_folder(&self, folder_id: &Uuid) -> Result<Response>;
+  fn get_folder_items(&self, folder_id: &Uuid) -> Result<Response>;
+  fn move_playlist_to_folder(&self, folder_id: &Uuid, playlist_id: &Uuid) -> Result<Response>;
+  fn rename_folder(&self, folder_id: &Uuid, name: &str) -> Result<Response>;
 }
