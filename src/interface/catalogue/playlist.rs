3ebd9778b74c855675e1f3523cf1d60f86450565
Autumn <autumn@puffin.place>
2025-08-06 03:36:03 +1000

    fix PlaylistCatalogue methods' signatures

--- src/interface/catalogue/playlist.rs
+++ src/interface/catalogue/playlist.rs
@@ -1,4 +1,4 @@
-use reqwest::Response;
+use reqwest::blocking::Response;
 use uuid::Uuid;
 
 use crate::{api, Result};
@@ -12,18 +12,19 @@ pub trait PlaylistCatalogue: Catalogue {
 }
 
 pub trait PlaylistCollection {
-  fn add_favorite_playlist(&self, playlist_id: &Uuid) -> Result<Response>;
-  fn create_playlist(&self, name: &str, description: &str, is_public: bool) -> Result<Response>;
+  fn add_favorite_playlist(&self, parent_folder_id: Option<&Uuid>, playlist_id: &Uuid) -> Result<Response>;
+  fn create_playlist(&self, parent_folder_id: Option<&Uuid>, name: &str, description: &str, is_public: bool) -> Result<Response>;
   fn remove_playlist(&self, playlist_id: &Uuid) -> Result<Response>;
-  fn move_playlist(&self, folder_id: &Uuid, playlist_id: &Uuid) -> Result<Response>;
+  fn move_playlist(&self, parent_folder_id: Option<&Uuid>, playlist_id: &Uuid) -> Result<Response>;
   fn edit_playlist(&self, playlist_id: &Uuid, name: &str, description: &str) -> Result<Response>;
   fn publish_playlist(&self, playlist_id: &Uuid) -> Result<Response>;
   fn unpublish_playlist(&self, playlist_id: &Uuid) -> Result<Response>;
   fn get_collection_playlist(&self, playlist_id: &Uuid) -> Result<Response>;
 
-  fn create_folder(&self, name: &str) -> Result<Response>;
+  // TODO: figure out what trns does here
+  fn create_folder(&self, parent_folder_id: Option<&Uuid>, name: &str, _trns: Option<&str>) -> Result<Response>;
   fn remove_folder(&self, folder_id: &Uuid) -> Result<Response>;
-  fn get_folder_items(&self, folder_id: &Uuid) -> Result<Response>;
-  fn move_playlist_to_folder(&self, folder_id: &Uuid, playlist_id: &Uuid) -> Result<Response>;
+  fn get_folder_items(&self, folder_id: Option<&Uuid>) -> Result<Response>;
+  fn move_folder(&self, parent_folder_id: Option<&Uuid>, folder_id: &Uuid) -> Result<Response>;
   fn rename_folder(&self, folder_id: &Uuid, name: &str) -> Result<Response>;
 }
