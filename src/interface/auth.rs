46e9d648ce569558c9730da6f8d90d792ef2a357
Autumn <autumn@puffin.place>
2025-08-11 22:14:25 +1000

    remove traits Auth and Credentials

--- src/interface/auth.rs
+++ src/interface/auth.rs
@@ -3,67 +3,9 @@
 //! This module contains the traits and structs for handling authentication with the Tidal API,
 //! allowing the user to implement as many or as few of the flows as they want
 
-use crate::Result;
-use chrono::Utc;
-
 pub mod flows;
 pub use flows::*;
-use isocountry::CountryCode;
 use serde::{Deserialize, Serialize};
-
-/// Access and manage authenticated credentials
-///
-/// Defines a client capable of being authenticated, required by other interfaces
-#[deprecated]
-pub trait Auth {
-  /// A type which acts as the credentials for the client
-  type Credentials: Credentials;
-
-  /// Get a reference to the credentials
-  fn get_credentials(&self) -> Result<&Self::Credentials>;
-  /// Get a mutable reference to the credentials
-  fn get_credentials_mut(&mut self) -> Result<&mut Self::Credentials>;
-
-  /// Get the credentials, refreshing them if they are expired
-  fn get_credentials_refresh(&mut self) -> Result<&Self::Credentials> {
-    self.credentials_refresh()?;
-    self.get_credentials()
-  }
-  /// Get the credentials, forcing a refresh
-  fn get_credentials_force_refresh(&mut self) -> Result<&Self::Credentials> {
-    self.credentials_force_refresh()?;
-    self.get_credentials()
-  }
-  /// Refresh the credentials if they are expired
-  fn credentials_refresh(&mut self) -> Result<()> {
-    let credentials = self.get_credentials_mut()?;
-    let expire_time = credentials.expires_at();
-
-    let cur_time = Utc::now().timestamp();
-    if expire_time <= cur_time {
-      credentials.refresh()?;
-    }
-    Ok(())
-  }
-  /// Force a refresh of the credentials
-  fn credentials_force_refresh(&mut self) -> Result<()> {
-    self.get_credentials_mut()?.refresh()
-  }
-}
-
-/// Basic requirements for authenticated credentials
-///
-/// Defines a type which can act as credentials for an authenticated client, requires an
-/// implementation of the `RefreshFlow` trait
-#[deprecated]
-pub trait Credentials: RefreshFlow {
-  /// Retuns the time at which the credentials expire, formatted as a unix timestamp
-  fn expires_at(&self) -> i64;
-  /// Returns the country code of the user associated with the credentials, if any
-  fn country_code(&self) -> Option<&CountryCode>;
-  /// Returns the id of the user associated with the credentials, if any
-  fn user_id(&self) -> Option<&u64>;
-}
 
 /// The type of grant being used for authentication
 #[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq, Eq)]
--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -1,5 +1,5 @@
 //! Interfaces for clients interacting with the Tidal API
 pub mod auth;
-pub use auth::{Auth, ClientFlow, Credentials, DeviceFlow, RefreshFlow, UserFlow};
+pub use auth::{ClientFlow, DeviceFlow, RefreshFlow, UserFlow};
 pub mod catalogue;
 pub mod users;
