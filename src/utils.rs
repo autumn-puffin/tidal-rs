93cfcd81de992076678bf74a2fa0fdd96524179a
Autumn <autumn@puffin.place>
2025-08-21 23:15:27 +1000

    Tidy up of utils module

--- src/utils.rs
+++ src/utils.rs
@@ -1,68 +1,6 @@
-use crate::{
-  api::{GrantType, TokenResponse},
-  client::{AuthCreds, ClientCreds},
-  endpoints::Endpoint,
-  Result,
-};
 use base64::prelude::*;
 use rand::{thread_rng, Rng};
-use reqwest::{
-  blocking::{Client, RequestBuilder},
-  Method,
-};
 use sha2::{Digest, Sha256};
-
-pub enum RequestAuth<'a> {
-  Basic(&'a ClientCreds),
-  Bearer(&'a AuthCreds),
-}
-impl<'a> From<&'a ClientCreds> for RequestAuth<'a> {
-  fn from(creds: &'a ClientCreds) -> Self {
-    RequestAuth::Basic(creds)
-  }
-}
-impl<'a> From<&'a AuthCreds> for RequestAuth<'a> {
-  fn from(creds: &'a AuthCreds) -> Self {
-    RequestAuth::Bearer(creds)
-  }
-}
-
-pub fn request_helper<'a>(client: &Client, method: Method, endpoint: Endpoint, auth: impl Into<RequestAuth<'a>>) -> RequestBuilder {
-  let mut builder = client.request(method, endpoint.to_string());
-  builder = match auth.into() {
-    RequestAuth::Basic(creds) => builder.basic_auth(creds.id(), Some(creds.secret())),
-    RequestAuth::Bearer(creds) => builder.bearer_auth(creds.access_token()),
-  };
-
-  builder
-}
-pub fn post_request_helper<'a>(client: &Client, endpoint: Endpoint, auth: impl Into<RequestAuth<'a>>) -> RequestBuilder {
-  request_helper(client, Method::POST, endpoint, auth)
-}
-pub fn oauth_request_helper<'a>(
-  client: &Client,
-  endpoint: Endpoint,
-  grant: GrantType,
-  auth: impl Into<RequestAuth<'a>>,
-  extra_params: Option<&[(&str, &str)]>,
-) -> RequestBuilder {
-  let mut params = extra_params.unwrap_or_default().to_vec();
-  params.push(("grant_type", grant.as_str()));
-
-  let mut builder = post_request_helper(client, endpoint, auth);
-  builder = builder.form(&params);
-
-  builder
-}
-
-pub fn client_login_impl(http_client: &Client, client_credentials: &ClientCreds) -> Result<AuthCreds> {
-  let endpoint = Endpoint::OAuth2Token;
-  let grant = GrantType::ClientCredentials;
-
-  let res = oauth_request_helper(http_client, endpoint, grant, client_credentials, None).send()?;
-
-  Ok(AuthCreds::new(grant, client_credentials.clone(), res.json::<TokenResponse>()?))
-}
 
 /// Generate a new PKCE pair (challenge, verifier)
 pub fn new_pkce_pair() -> (String, String) {
@@ -87,7 +25,7 @@ pub fn res_to_error(res: reqwest::blocking::Response) -> crate::Error {
 #[allow(dead_code)]
 pub fn client_from_authfile() -> Option<crate::client::Client> {
   let creds = std::fs::read_to_string("./auth.json").ok()?;
-  let creds: AuthCreds = serde_json::from_str(&creds).ok()?;
+  let creds: crate::client::AuthCreds = serde_json::from_str(&creds).ok()?;
   let country = *creds.country_code().unwrap();
   let mut client = crate::client::Client::new(creds.client_credentials().clone());
   client.set_auth_credentials(creds);
